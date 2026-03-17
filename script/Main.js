3589ec473eb68bb51b4662fed4202ae419db401d
Rubinzo <rubinzopdp@gmail.com>
2026-03-17 20:02:43 +0100

    Oscuro_Hecho/Excepto_Footer

--- script/Main.js
+++ script/Main.js
@@ -31,11 +31,58 @@ fetch("/data/data.xml").then((response) => response.text())
     });
 
 const selector = document.getElementById("selector");
+const header = document.getElementById("header");
 
 selector.addEventListener("change", function(){
-    const info = document.getElementBy("info")
-    info.style.backgroundColor = "black";
-    
+    const info = document.getElementsByClassName("info");
+    const footer = document.getElementsByClassName("footer");
+    const estilos = document.getElementsByClassName("estilos");
+
+    if(this.value == "oscuro"){
+        //Header
+        header.style.backgroundColor = "#3D2B1F";
+        header.style.color = "white";
+        header.style.borderColor = "white";
+        //Background
+        document.body.style.backgroundColor = "black";
+        
+        /*#001F3F*/
+        //Tarjetas
+        for (let i = 0; i < info.length; i++) {
+            info[i].style.backgroundColor = "#3D2B1F";
+            info[i].style.color = "white";
+            info[i].style.borderColor = "white";
+        }   
+        //Footer
+        footer.style.backgroundColor = "#0F172A";
+        footer.style.borderColor = "white";
+        estilos.style.backgroundColor = "#0F172A";
+        estilos.style.borderColor = "white";
+
+    }
+
+    if(this.value == "claro"){
+        //Header
+        header.style.backgroundColor = "beige";
+        header.style.color = "black";
+        header.style.borderColor = "black";
+        //Background
+        document.body.style.backgroundColor = "azure";
+        
+        /*#001F3F*/
+        //Tarjetas
+        for (let i = 0; i < info.length; i++) {
+            info[i].style.backgroundColor = "beige";
+            info[i].style.color = "black";
+            info[i].style.borderColor = "black";
+        }   
+        //Footer
+        footer.style.backgroundColor = "#475569";
+        footer.style.borderColor = "black";
+        estilos.style.backgroundColor = "#475569";
+        estilos.style.borderColor = "black";
+
+    }
 });
 
 
